4a3f24a817515284587c366dbff013ab09664f4f
Nick <nickjbianco25@gmail.com>
2020-10-26 17:50:55 -0400

    React setup

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get pages_index_url
     assert_response :success
   end
 
