1305894d9413fac71868fa057cc4d2e565f0c249
Nick <nickjbianco25@gmail.com>
2020-10-27 11:00:27 -0400

    search bar

--- app/javascript/components/HeaderPage.js
+++ app/javascript/components/HeaderPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default () => {
+    const [searchText, setSearchText] = useState('')
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        setSearchText(e.target.value)
+        console.log(searchText)
+        setSearchText('')
+    }
+
     return (
         <div>
+            <form onSubmit={handleSearch}>
+                <input 
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+                <button>Search</button>
+            </form>
             <NavLink to="/" exact={true} activeClassName="is-active" >Home</NavLink>
             <NavLink to="/mynetwork" activeClassName="is-active" >My Network</NavLink>
             <NavLink to="/jobs" activeClassName="is-active" >Jobs</NavLink>
