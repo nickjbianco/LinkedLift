673740b5490972407beb52e6f23e058650dee7ff
Nick <nickjbianco25@gmail.com>
2020-10-27 11:49:21 -0400

    one-change

--- app/javascript/components/LinkedLiftApp-components/MyAccountInfo.js
+++ app/javascript/components/LinkedLiftApp-components/MyAccountInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
     return (
         <div>
-            <p>image here</p>
+            <p>your image here</p>
             <h3>Name</h3>
             <p>Title at Gym</p>
             <p>Number of connections</p>
