a0b4a05a66541eff4267f2c686f88be60582339b
Nick <nickjbianco25@gmail.com>
2020-10-28 19:15:43 -0400

    fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :title, presence: true 
-  validates :location, presence: true 
   has_many :employments
   has_many :gyms, through: :employments
 end
