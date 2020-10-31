2aa366cec0ac213b249dd05dbbd9e65e5536211f
Nick <nickjbianco25@gmail.com>
2020-10-31 11:48:03 -0400

    yes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
   has_many :employments
   has_many :gyms, through: :employments
 end
