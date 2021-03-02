36693bea775af607184fd9eb023b86f30cd6b684
Nick <nickjbianco25@gmail.com>
2021-03-02 09:59:14 -0500

    Comment out DevTools in Compose function

--- app/javascript/store/myStore.js
+++ app/javascript/store/myStore.js
@@ -16,17 +16,10 @@ const allReducers = combineReducers({
   viewUser: ViewUserReducer,
 });
 
-const devTools =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
-
 export default createStore(
   allReducers,
   compose(
-    applyMiddleware(thunk),
-    devTools
+    applyMiddleware(thunk)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
